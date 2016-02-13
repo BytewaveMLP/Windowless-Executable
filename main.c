e8e8dc31b41a9a37b1b039b389e0f4f058bcfeea
Eliot Partridge <bytewave@ponypwnage.com>
2016-02-13 14:49:18 -0600

    Use spaces instead of tabs for parameter alignment

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <windows.h>
 
 int APIENTRY WinMain(HINSTANCE hInstance,
-					 HINSTANCE hPrevInstance,
-					 LPTSTR    lpCmdLine,
-					 int       nCmdShow)
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
 {
 	while (1) {
 		Sleep(24 * 60 * 60 * 1000);
