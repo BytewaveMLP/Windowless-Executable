dddf5aaeee21b5bb6a8abd4a91be07cc5ba5f82b
Alexandra F <thunderlive-sunshine@live.com>
2016-02-13 16:04:16 -0500

    Insert commit description here
    Insert commit extended description here

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <windows.h>
+#include <limits.h>
 
 int APIENTRY WinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPTSTR    lpCmdLine,
                      int       nCmdShow) {
 	while (1) {
-		Sleep(24 * 60 * 60 * 1000); // huh? loop? nah, I'll do that tomorrow
+		Sleep(UINT_MAX); // huh? loop? nah, I'll do that tomorrow
 	}
 
 	return 255; // we shouldn't be here, why are we here?
